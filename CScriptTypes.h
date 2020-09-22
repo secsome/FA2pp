c080ad2add66d08cf121c916c385075d433856f2
secsome <302702960@qq.com>
2020-09-22 23:58:21 +0800

    just remove a useless headfile

--- CScriptTypes.h
+++ CScriptTypes.h
@@ -2,8 +2,6 @@
 
 #include "FA2PP.h"
 
-#include <AFXOLE.H>
-
 class NOVTABLE CScriptTypes : public FA2CDialog
 {
 public:
@@ -17,4 +15,65 @@ public:
 	CComboBox	CCBScriptParameter; // 332
 	CListBox		CLBScriptActions; // 392
 	CString			CString_ScriptName; // 452
+
+public:
+	virtual BOOL OnInitDialog() override
+	{
+		JMP_THIS(0x4D8D20);
+	}
+
+	virtual void OnScriptSelChanged()
+	{
+		JMP_THIS(0x4D61B0);
+	}
+
+	virtual void OnActionSelChanged()
+	{
+		JMP_THIS(0x4D6500);
+	}
+
+	virtual void OnScriptSelUpdateName()
+	{
+		JMP_THIS(0x4D6770);
+	}
+
+	virtual void OnParamConvert()
+	{
+		JMP_THIS(0x4D6A10);
+	}
+
+	virtual void OnParamEntered()
+	{
+		JMP_THIS(0x4D75D0);
+	}
+
+	virtual void OnUpdateAction()
+	{
+		JMP_THIS(0x4D7670);
+	}
+
+	virtual void OnParamValidation()
+	{
+		JMP_THIS(0x4D7A50);
+	}
+
+	virtual void OnNameUpdate() // maybe?
+	{
+		JMP_THIS(0x4D7AC0);
+	}
+
+	virtual void OnHandleAction() // delete / add?
+	{
+		JMP_THIS(0x4D7DD0);
+	}
+
+	virtual void OnLoadMapScripts()
+	{
+		JMP_THIS(0x4D8390);
+	}
+
+	virtual void OnDeleteScript()
+	{
+		JMP_THIS(0x4D8730);
+	}
 };
