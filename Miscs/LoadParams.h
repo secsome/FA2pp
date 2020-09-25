1c828d2fd11b6d6a826d044ba19b2f53b62aefd2
secsome <302702960@qq.com>
2020-09-25 13:39:07 +0800

    jmp->JMP_STD

--- Miscs/LoadParams.h
+++ Miscs/LoadParams.h
@@ -2,99 +2,96 @@
 
 #include "../FA2PP.h"
 
-namespace FA2PP {
-    namespace Miscs{
-        namespace LoadParams {
-            void LoadToCombobox(CComboBox* pComboBox, int nParamCode) {
-                JMP(0x43CE50);
-            }
 
-            void BuildingTypes(CComboBox* pComboBox, bool bDisplayAsName){
-                JMP(0x43ED50);
-            }
+namespace Miscs {
+    namespace LoadParams {
+        void LoadToCombobox(CComboBox* pComboBox, int nParamCode) {
+            JMP_STD(0x43CE50);
+        }
 
-            void InfantryTypes(CComboBox* pComboBox) {
-                JMP(0x43F2C0);
-            }
+        void BuildingTypes(CComboBox* pComboBox, bool bDisplayAsName) {
+            JMP_STD(0x43ED50);
+        }
 
-            void VehicleTypes(CComboBox* pComboBox) {
-                JMP(0x63F760);
-            }
+        void InfantryTypes(CComboBox* pComboBox) {
+            JMP_STD(0x43F2C0);
+        }
 
-            void AircraftTypes(CComboBox* pComboBox) {
-                JMP(0x43FC00);
-            }
+        void VehicleTypes(CComboBox* pComboBox) {
+            JMP_STD(0x63F760);
+        }
 
-            void TechnoTypes(CComboBox* pComboBox) {
-                JMP(0x4400A0);
-            }
+        void AircraftTypes(CComboBox* pComboBox) {
+            JMP_STD(0x43FC00);
+        }
 
-            void LocalVariables(CComboBox* pComboBox) {
-                JMP(0x441140);
-            }
+        void TechnoTypes(CComboBox* pComboBox) {
+            JMP_STD(0x4400A0);
+        }
 
-            void GlobalVariables(CComboBox* pComboBox) {
-                JMP(0x441530);
-            }
+        void LocalVariables(CComboBox* pComboBox) {
+            JMP_STD(0x441140);
+        }
 
-            void TutorialTexts(CComboBox* pComboBox) {
-                JMP(0x441910);
-            }
+        void GlobalVariables(CComboBox* pComboBox) {
+            JMP_STD(0x441530);
+        }
 
-            void Triggers(CComboBox* pComboBox) {
-                JMP(0x441A40);
-            }
+        void TutorialTexts(CComboBox* pComboBox) {
+            JMP_STD(0x441910);
+        }
 
-            void YesNo(CComboBox* pComboBox) {
-                JMP(0x441E00);
-            }
+        void Triggers(CComboBox* pComboBox) {
+            JMP_STD(0x441A40);
+        }
 
-            void Sounds(CComboBox* pComboBox) {
-                JMP(0x441F20);
-            }
+        void YesNo(CComboBox* pComboBox) {
+            JMP_STD(0x441E00);
+        }
 
-            void Themes(CComboBox* pComboBox) {
-                JMP(0x442240);
-            }
+        void Sounds(CComboBox* pComboBox) {
+            JMP_STD(0x441F20);
+        }
 
-            void Speeches(CComboBox* pComboBox) {
-                JMP(0x442740);
-            }
+        void Themes(CComboBox* pComboBox) {
+            JMP_STD(0x442240);
+        }
 
-            void SuperWeaponTypes(CComboBox* pComboBox) {
-                JMP(0x442A60);
-            }
+        void Speeches(CComboBox* pComboBox) {
+            JMP_STD(0x442740);
+        }
 
-            void Animations(CComboBox* pComboBox) {
-                JMP(0x442E50);
-            }
+        void SuperWeaponTypes(CComboBox* pComboBox) {
+            JMP_STD(0x442A60);
+        }
 
-            void Particles(CComboBox* pComboBox) {
-                JMP(0x443240);
-            }
+        void Animations(CComboBox* pComboBox) {
+            JMP_STD(0x442E50);
+        }
 
-            void Movies(CComboBox* pComboBox, bool arg4, bool a3) {
-                JMP(0x443630);
-            }
+        void Particles(CComboBox* pComboBox) {
+            JMP_STD(0x443240);
+        }
 
-            void Movies(CComboBox* pComboBox, bool bShowIndex) {
-                Movies(pComboBox, ~bShowIndex, bShowIndex);
-            }
+        void Movies(CComboBox* pComboBox, bool arg4, bool a3) {
+            JMP_STD(0x443630);
+        }
 
-            void Houses(CComboBox* pComboBox, 
-                bool b1, bool b2, bool b3) {
-                JMP(0x4448F0);
-            }
+        void Movies(CComboBox* pComboBox, bool bShowIndex) {
+            Movies(pComboBox, ~bShowIndex, bShowIndex);
+        }
 
-            void TeamTypes(CComboBox* pComboBox, bool bLeaveNone) {
-                JMP(0x445B30);
-            }
+        void Houses(CComboBox* pComboBox,
+            bool b1, bool b2, bool b3) {
+            JMP_STD(0x4448F0);
+        }
 
-            void Waypoints(CComboBox* pComboBox) {
-                JMP(0x446070);
-            }
+        void TeamTypes(CComboBox* pComboBox, bool bLeaveNone) {
+            JMP_STD(0x445B30);
+        }
 
-
+        void Waypoints(CComboBox* pComboBox) {
+            JMP_STD(0x446070);
         }
     }
 }
