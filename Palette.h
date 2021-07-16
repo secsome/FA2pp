4c24d4e562a8de66200a2e2793c4c6e3467abacd
secsome <302702960@qq.com>
2021-07-16 16:48:34 +0800

    Add Palette defiitions

--- Palette.h
+++ Palette.h
@@ -2,7 +2,30 @@
 
 #include "FA2PP.h"
 
+struct ColorStruct
+{
+    unsigned char red, green, blue;
+};
+
+class BytePalette
+{
+public:
+    ColorStruct Data[256];
+
+    ColorStruct& operator[](int index) { return Data[index]; }
+};
+
 class Palette
 {
+public:
+    COLORREF Data[256];
 
+    COLORREF& operator[](int index) { return Data[index]; }
+    ColorStruct GetByteColor(int index) {
+        ColorStruct ret;
+        ret.red = GetRValue(Data[index]);
+        ret.green = GetGValue(Data[index]);
+        ret.blue = GetBValue(Data[index]);
+        return ret;
+    }
 };
