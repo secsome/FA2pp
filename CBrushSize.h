c849d5920585b1fcf2fb0d49962bb45a43785d24
secsome <302702960@qq.com>
2021-11-22 00:25:40 +0800

    Update CBrushSize

--- CBrushSize.h
+++ CBrushSize.h
@@ -7,6 +7,8 @@ class NOVTABLE CBrushSize : public CDialogBar
 public:
 	CBrushSize() = default;
 
+	static void __stdcall UpdateBrushSize(unsigned int nTileset) { JMP_STD(0x523350); }
+
 	//member properties
 	int nCurSel; // ComboBox 1377
 };
