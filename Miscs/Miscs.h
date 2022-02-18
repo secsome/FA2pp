2a136290a05d79223ca4a1b343f68b2f16356127
secsome <302702960@qq.com>
2022-02-18 17:35:08 +0800

    Add Miscs::ParseHouseName

--- Miscs/Miscs.h
+++ Miscs/Miscs.h
@@ -3,11 +3,17 @@
 #include "../FA2PP.h"
 
 namespace Miscs {
-    void TrimIndex(ppmfc::CString& string) {
-        string.TrimLeft();
-        string.TrimRight();
-        int spaceIndex = string.Find(' ');
-        if (spaceIndex > 0)
-            string = string.Mid(0, spaceIndex);
+    ppmfc::CString* __cdecl ParseHouseName(ppmfc::CString* pRet, ppmfc::CString src, bool bRealToUIName) JMP_STD(0x43C3C0);
+    ppmfc::CString ParseHouseName(const ppmfc::CString& src, bool bRealToUIName) 
+    { 
+        ppmfc::CString buffer; 
+        ParseHouseName(&buffer, src, bRealToUIName);
+        return buffer;
+    }
+    ppmfc::CString ParseHouseName(ppmfc::CString&& src, bool bRealToUIName)
+    {
+        ppmfc::CString buffer;
+        ParseHouseName(&buffer, src, bRealToUIName);
+        return buffer;
     }
 }
